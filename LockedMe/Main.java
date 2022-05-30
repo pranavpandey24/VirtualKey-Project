4e0aeb3a2262abc593dad460417f533f8ebcb716
Pranav <pranavpandey829@gmail.com>
2022-05-30 00:38:10 -0400

    Version 1

---
+++ LockedMe/Main.java
@@ -0,0 +1,15 @@
+package com.project.lockedme;
+
+public class Main {
+	
+		
+		/*Enter your desired Directory path */
+		public static final String path = "C:\\Users\\Consultadd\\Main"; 
+		
+		public static void main(String[] args) {
+			Menus menu = new Menus();
+			menu.introScreen();
+			menu.mainMenu();
+		}
+		
+	}
